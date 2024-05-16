92559a869f7b2ac8b9ca2e2b28ecd1e477000cbf
Nicholas Hilaire <131618569+Hilaire007@users.noreply.github.com>
2024-05-16 17:44:33 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,104 @@
+async function moduleProject4() {
+  // 👇 WORK WORK BELOW THIS LINE 👇
+  const footer = document.querySelector("footer");
+  const currentYear = new Date().getFullYear();
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`;
+
+  let descriptions = [
+    ["Sunny", "☀️"],
+    ["Cloudy", "☁️"],
+    ["Rainy", "🌧️"],
+    ["Thunderstorm", "⛈️"],
+    ["Snowy", "❄️"],
+    ["Partly Cloudy", "⛅️"],
+  ];
+
+  // 👉 Tasks 1 - 5 go here
+  // Task 1:
+  // const weatherDisplay = weatherWidget.style.display = "none"
+  document.querySelector("#weatherWidget").style.display = "none";
+  //Task 2:
+  document
+    .querySelector("#citySelect")
+    .addEventListener("change", async (evt) => {
+      console.log("city selection changed");
+      try {
+        document
+          .querySelector("#citySelect")
+          .setAttribute("disabled", "disabled");
+        document.querySelector("#weatherWidget").style.display = "none";
+        document.querySelector(".info").textContent =
+          "Fetching weather data...";
+
+        console.log(evt.target.value);
+        let city = evt.target.value;
+        let url = `http://localhost:3003/api/weather?city=${city}`;
+
+        const res = await axios.get(url);
+
+        document.querySelector("#weatherWidget").style.display = "block";
+        document.querySelector(".info").textContent = "";
+        evt.target.removeAttribute("disabled");
+
+        let { data } = res;
+        document.querySelector(
+          "#apparentTemp div:nth-child(2)"
+        ).textContent = `${data.current.apparent_temperature}°`;
+        document.querySelector("#todayDescription").textContent =
+          descriptions.find(
+            (d) => d[0] === data.current.weather_description
+          )[1];
+        document.querySelector(
+          "#todayStats div:nth-child(1)"
+        ).textContent = `${data.current.temperature_min}°/${data.current.temperature_max}°`;
+        document.querySelector(
+          "#todayStats div:nth-child(2)"
+        ).textContent = `Precipitation: ${
+          data.current.precipitation_probability * 100
+        }%`;
+        document.querySelector(
+          "#todayStats div:nth-child(3)"
+        ).textContent = `Humidity: ${data.current.humidity}%`;
+        document.querySelector(
+          "#todayStats div:nth-child(4)"
+        ).textContent = `Wind: ${data.current.wind_speed}m/s`;
+
+        data.forecast.daily.forEach((day, idx) => {
+          let card = document.querySelectorAll(".next-day")[idx];
+
+          let weekDay = card.children[0];
+          let apparent = card.children[1];
+          let minMax = card.children[2];
+          let precipitation = card.children[3];
+
+          weekDay.textContent = day.date;
+          // day.date.moment('2023-05-25', '2023-05-26', '2023-05-27').format('dddd') (To fix the date formatting maybe?)
+          // Detroit and the temperatires still end up being hard coded with the above added.
+          apparent.textContent = descriptions.find(
+            (d) => d[0] === day.weather_description
+          )[1];
+          minMax.textContent = `${day.temperature_min}°/${day.temperature_max}°`;
+          precipitation.textContent = `Precipitation: ${
+            day.precipitation_probability * 100
+          }%`;
+        });
+
+        document.querySelector("#location").firstElementChild.textContent =
+          data.location.city;
+      } catch (error) {
+        console.log(
+          "Promise rejected with an error.message -->",
+          error.message
+        );
+      }
+    });
+
+  // 👆 WORK WORK ABOVE THIS LINE 👆
+}
+
+// ❗ DO NOT CHANGE THE CODE  BELOW
+// ❗ DO NOT CHANGE THE CODE  BELOW
+// ❗ DO NOT CHANGE THE CODE  BELOW
+if (typeof module !== "undefined" && module.exports)
+  module.exports = { moduleProject4 };
+else moduleProject4();
